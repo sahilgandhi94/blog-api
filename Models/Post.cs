680456fda65e57b8047d5ea6bff1f702dad1bc14
Sahil Gandhi <sahilgandhi94@gmail.com>
2018-03-11 18:04:14 -0400

    controller, model and updated data

---
+++ Models/Post.cs
@@ -0,0 +1,12 @@
+
+
+namespace monster_assignment.Models
+{
+    public class Post
+    {
+        public int id;
+        public int userId;
+        public string title;
+        public string body;
+    }
+}
